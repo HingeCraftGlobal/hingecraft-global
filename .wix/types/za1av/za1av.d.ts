2b9a3ec956bf9909d2bd561822a406d0d884e35b
William Ferguson <chandlerferguson319@gmail.com>
2025-12-10 14:26:55 -0700

    Stage all changes before history cleanup

--- .wix/types/za1av/za1av.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="../masterPage/masterPage.d.ts" />
-type PageElementsMap = MasterPageElementsMap & {
-	"#section1": $w.Section;
-	"#legalContent": $w.HtmlComponent;
-	"#page1": $w.Page;
-}
